c49ceaedce561afb1899b07eb1a254cd460c654b
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-11-18 22:31:53 +0200

    ready-hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,111 @@
 const express = require('express')
+
+const { NotFound, BadRequest } = require('http-errors')
+const Joi = require('joi')
+
+const joiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required()
+})
 const router = express.Router()
 
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact
+} = require('../../model/index')
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await listContacts()
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { result }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const result = await getContactById(contactId)
+    if (!result) {
+      throw new NotFound(`Product with id=${contactId} not found`)
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        result
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = joiSchema.validate(req.body)
+    if (error) {
+      throw new BadRequest(error.message)
+    }
+    const result = await addContact(req.body)
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: {
+        result
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const result = await removeContact(contactId)
+    if (!result) {
+      throw new NotFound(`Product with id=${contactId} not found`)
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      message: 'Remove success'
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', async (req, res, next) => {
+  try {
+    if (req.body === {}) {
+      throw new NotFound('missing fields')
+    }
+    const { contactId } = req.params
+    const result = await updateContact(contactId, req.body)
+    if (!result) {
+      throw new NotFound(`Product with id=${contactId} not found`)
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        result
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
