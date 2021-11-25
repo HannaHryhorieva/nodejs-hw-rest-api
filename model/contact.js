47a3b6f4ae4c7855d41700175d5a4f4d327cb408
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-11-25 14:18:49 +0200

    hw-03 quest

---
+++ model/contact.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact']
+    },
+    email: {
+      type: String
+    },
+    phone: {
+      type: String
+    },
+    favorite: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { versionKey: false, timestamps: true }
+)
+const Contact = mongoose.model('contact', contactSchema)
+
+const joiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean().required()
+})
+
+module.exports = { Contact, joiSchema }
